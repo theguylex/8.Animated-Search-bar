7dd06332ca26da3e1f526fdd244d76a05809607a
Chinonye Ekpendu <alexekpendu001@gmail.com>
2025-05-18 15:45:20 +0100

    searchbtn: bg + colour

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const searchBtn = document.querySelector(".search-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
-// const searchBtn = document.querySelector(".search-btn");
 const searchBox = document.querySelector(".search-box");
+const searchInput = document.querySelector("input");
+
 
 searchBtn.onclick = () => {
     searchBox.classList.toggle("active");
